87dc95ee4a3b04166c5d265db7fcad71af12f834
Madeline Celis <celiscb@vcu.edu>
2018-09-25 17:58:25 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,33 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+function init(){
+  let compareArray = [];
+  let compareString = '';
+  let codeString = '';
+
+  for(let i = 0; i < codes.length; i++){
+    codeString += codes[i];
+  }
+
+
+  document.body.addEventListener('keydown', function(event){
+    compareArray.push(event.key);
+
+    if (compareArray.length <= 10){
+      for(let i = 0; i < compareArray.length; i++){
+        if(i === 9){
+          compareString += compareArray;
+        }
+      }
+    }else{
+      compareArray = [];
+      compareString = '';
+    }
+    compareString = compareString.split(',').join('');
+
+    if(compareString === codeString){
+      alert('Congratulations!');
+    }
+  });
 }
